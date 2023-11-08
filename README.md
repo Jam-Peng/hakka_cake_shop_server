668bbb2dff8355a4fa9416089615c20f5f9c2bbd
Jam Peng <pengzhe.info@gmail.com>
2023-11-08 16:19:22 +0800

    新增 README 文件

---
+++ README.md
@@ -0,0 +1,127 @@
+
+<div align="center">
+<img width="20%" src="./public/logo.png">
+
+# 客家糕粿店 - 網站全端專案
+</div>
+
+> ⚠ 測試請同時下載前、後台資料包 : <br>
+Frontend --> https://github.com/Jam-Peng/hakka_cake_shop_frontend <br>
+Backend --> https://github.com/Jam-Peng/hakka_cake_shop_backend
+###  Preview :
+
+<table width="100%"> 
+<tr>
+<td width="50%">      
+&nbsp; 
+<br>
+<p align="center">
+  Frontend | Home
+</p>
+<center>
+<img src="./public/front_home.png">
+</td>
+
+<td width="50%">
+<br>
+<p align="center">
+  Backend | Chart
+</p>
+<center>
+<img src="./public/back_chart.png">
+</td>
+</tr>
+</table>
+
+#
+
+## 設計動機
+- 幫舅舅的客家手作糕點設計一個網路商店與後台管理系統。。
+- 使用 Django 撰寫 API 將前、後端做資料的整合與應用。
+- <a href="https://drive.google.com/file/d/1MIN3X9e8rf6s-wNaw6T52tyrQcTn7GsT/view?usp=sharing" target="_blank">開啟線上完整專案介紹</a>
+
+
+#
+### 使用環境
+- `Python-3`。
+
+---
+### 使用技術
+- 以 `Django` 框架開發。
+- 使用 `Django REST framework (DRF)` 建立、發布與管理 Web API，建立序列化器 (serializer) 轉換 JSON 格式，路由匯入 DefaultRouter 管理 API URL，視圖 (views) 使用 viewsets 和 APIView 根據功能需求的不同，設定適合使用的 API 方式。
+- 使用 `Django REST framework SimpleJWT` 設定使用者登入時接收 JSON Web Token 執行身份的驗證及根據 refresh token 自動發送更新 JWT 。
+
+### 使用套件
+- `django`
+- `django-cors-headers`
+- `djangorestframework`
+- `djangorestframework-simplejwt`
+- `Pillow`
+- `uuid`
+- `datetime`
+
+
+## 如何執行 - `前、後台`
+--> 使用 Zip 下載專案或使用下面的指令下載
+```bash
+git clone https://github.com/Jam-Peng/hakka_cake_shop_frontend
+
+git clone https://github.com/Jam-Peng/hakka_cake_shop_backend
+```
+
+--> 進入前台資料包
+```bash
+cd frontend
+```
+
+--> 或進入後台資料包
+```bash
+cd backendend
+```
+
+--> 安裝專案需求 dependencies
+```bash
+npm install
+```
+
+--> 執行專案
+```bash
+npm run start
+```
+
+## 如何執行 - `伺服器`
+--> 使用 Zip 下載專案或使用下面的指令下載
+```bash
+git clone https://github.com/Jam-Peng/hakka_cake_shop_server.git
+```
+
+--> 進入server資料包
+```bash
+cd server
+```
+
+--> 建立虛擬環境
+```bash
+conda create --name venv python=3.9
+```
+
+--> 執行虛擬環境
+```bash
+conda activate venv
+```
+
+--> 安裝套件
+```bash
+pip install -r requirements.txt
+```
+
+--> 數據庫遷移
+```bash
+python manage.py makemigrations
+python manage.py migrate
+```
+
+--> 運行伺服器
+```bash
+python manage.py runserver
+```
--- product/views.py
+++ product/views.py
@@ -17,63 +17,65 @@ from django.core.files.uploadedfile import InMemoryUploadedFile   # 檢查是否
 def getRoutes(request):
     routes = [
         {
-            'GET | api/v1/',
+            '查詢所有API| GET | api/v1/',
         },
         '===================  使用者登入_Token驗證  ===================',
         {
-            'POST | api/v1/token/',
-            'POST | api/v1/token/refresh/',
+            '使用者登入| POST | api/v1/token/',
+            '更新Token| POST | api/v1/token/refresh/',
         },
         '====================  後台使用  ====================',
         {
-            'POST | api/v1/staff_set/',                          # 員工註冊
-            'PUT | api/v1/staff_set/:id/',                       # 更新帳號
-            'POST | api/v1/clock-in/:id/',                       # 上班打卡
-            'PUT | api/v1/clock-out/:id/',                       # 下班打卡
-            'GET | api/v1/staffs/',                              # 取得全部員工
-            'GET | api/v1/staffs/search/?search=query',          # 搜尋員工
-            'GET | api/v1/staff_clock_in_out_records/',          # 取得當年度員工每個月的上下班紀錄
-            'POST | api/v1/staff_one_month_clock_records/:id/',  # 查詢員工特定月份的上下班紀錄
+            '員工註冊| POST | api/v1/staff_set/',                            # 員工註冊
+            '更新帳號| PUT | api/v1/staff_set/:id/',                         # 更新帳號
+            '上班打卡| POST | api/v1/clock-in/:id/',                         # 上班打卡
+            '下班打卡| PUT | api/v1/clock-out/:id/',                         # 下班打卡
+            '取得全部員工| GET | api/v1/staffs/',                             # 取得全部員工
+            '搜尋員工| GET | api/v1/staffs/search/?search=query',            # 搜尋員工
+            '取得每個月打卡紀錄| GET | api/v1/staff_clock_in_out_records/',    # 取得當年度員工每個月的上下班紀錄
+            '查詢員工特定月份打卡紀錄| POST | api/v1/staff_one_month_clock_records/:id/',  # 查詢員工特定月份的上下班紀錄
 
-            'PATCH | api/v1/staff_delete/:id/',               # 將員工 is_delete設為 True
-            'GET | api/v1/staff_wait_set/',                   # 取得加入待刪除的所有員工
-            'PUT | api/v1/staff_wait_set/:id/',               # 取回員工
-            'DELETE | api/v1/staff_delete_from_db/:id/',      # 刪除員工(從資料庫中刪除)
+            '將員工加入待刪除| PATCH | api/v1/staff_delete/:id/',             # 將員工 is_delete設為 True
+            '取得待刪除所有員工| GET | api/v1/staff_wait_set/',               # 取得加入待刪除的所有員工
+            '取回員工| PUT | api/v1/staff_wait_set/:id/',                   # 取回員工
+            '刪除員工| DELETE | api/v1/staff_delete_from_db/:id/',          # 從資料庫中刪除員工
         },
         {
-            'GET | api/v1/back_client_set/',                  # 取得所有會員(除了系統管理員)
-            'PATCH | api/v1/client_delete/:id/',              # 將會員加入黑名單
-            'GET | api/v1/client_black_set',                  # 取得加入黑名單的所有會員
-            'PUT | api/v1/client_black_set/:id/',             # 取回會員
-            'DELETE | api/v1/client_delete_from_db/:id/',     # 刪除會員(從資料庫中刪除)
-            'GET | api/v1/client/search/?search=query',       # 搜尋員工
+            # 取得所有會員(除了系統工程師)
+            '取得所有會員| GET | api/v1/back_client_set/',
+            '將會員加入黑名單| PATCH | api/v1/client_delete/:id/',            # 將會員加入黑名單
+            '取得黑名單的所有會員| GET | api/v1/client_black_set',             # 取得加入黑名單的所有會員
+            '取回會員| PUT | api/v1/client_black_set/:id/',                  # 取回會員
+            '刪除會員| DELETE | api/v1/client_delete_from_db/:id/',          # 從資料庫中刪除會員
+            '搜尋會員| GET | api/v1/client/search/?search=query',            # 搜尋會員
         },
         {
-            'GET | api/v1/products/',                         # 取得全部商品
-            'POST | api/v1/product_set/',                     # 建立商品
-            'PUT | api/v1/product_set/:id/',                  # 更新商品
-            'DELETE | api/v1/product_delete/:id/',            # 刪除商品
-            'PATCH | api/v1/product_show/:id/',               # 更新商品是否顯示於前台
+            '取得全部商品| GET | api/v1/products/',                      # 取得全部商品
+            '建立商品| POST | api/v1/product_set/',                     # 建立商品
+            '更新商品| PUT | api/v1/product_set/:id/',                  # 更新商品
+            '刪除商品| DELETE | api/v1/product_delete/:id/',            # 刪除商品
+            '商品是否顯示於前台| PATCH | api/v1/product_show/:id/',       # 更新商品是否顯示於前台
         },
         {
-            'GET | api/v1/all_orders',                        # 取得所有訂單
-            'DELETE | api/v1/delete_order/:id/',              # 刪除一筆訂單
-            'GET | api/v1/order/search/?search=query/',       # 訂單查詢
+            '取得所有訂單| GET | api/v1/all_orders',                      # 取得所有訂單
+            '刪除一筆訂單| DELETE | api/v1/delete_order/:id/',            # 刪除一筆訂單
+            '訂單查詢| GET | api/v1/order/search/?search=query/',        # 訂單查詢
         },
         {
-            'GET | /api/v1/all_daily_order_stats/',           # 取得所有日期訂單統計(未使用)
-            'GET | /api/v1/daily_order_stats/',               # 取得當日訂單統計
-            'GET | /api/v1/monthly_order_stats/',             # 取得每月訂單統計
-            'GET | /api/v1/yearly_order_stats/',              # 取得當年度訂單統計
-            'POST | /api/v1/search_date_order_stats/',        # 搜尋日期訂單統計
+            # 取得所有日期訂單統計(未使用)
+            '取得所有日期訂單統計| GET | /api/v1/all_daily_order_stats/',
+            '取得當日訂單統計| GET | /api/v1/daily_order_stats/',             # 取得當日訂單統計
+            '取得每月訂單統計| GET | /api/v1/monthly_order_stats/',           # 取得每月訂單統計
+            '取得當年度訂單統計| GET | /api/v1/yearly_order_stats/',          # 取得當年度訂單統計
+            '搜尋日期訂單統計| POST | /api/v1/search_date_order_stats/',      # 搜尋日期訂單統計
         },
 
         '===================  前台使用  ====================',
-        'POST | api/v1/client_set/',                          # 會員註冊帳號
-        'PUT | api/v1/client_update/:id/',                    # 更新會員資料
-        'GET | api/v1/front_products/',                       # 取得所有商品
-        'POST | api/v1/front_order/',                         # 建立商品訂單
-        'GET | api/v1/user_orders/:id/',                      # 取得一個客戶所有訂單
+        '會員註冊帳號| POST | api/v1/client_set/',                          # 會員註冊帳號
+        '更新會員資料| PUT | api/v1/client_update/:id/',                    # 更新會員資料
+        '取得所有商品| GET | api/v1/front_products/',                       # 取得所有商品
+        '建立商品訂單| POST | api/v1/front_order/',                         # 建立商品訂單
+        '取得單一客戶所有訂單| GET | api/v1/user_orders/:id/',               # 取得一個客戶所有訂單
     ]
     return Response(routes)
 
